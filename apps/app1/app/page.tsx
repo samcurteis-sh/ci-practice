db60dfb17da98e8c92e306ba6ae052b7524f5716
samcurteis-sh <146755918+samcurteis-sh@users.noreply.github.com>
2025-08-14 12:15:53 +0100

    ci: testing env vars

--- apps/app1/app/page.tsx
+++ apps/app1/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     setSaysHello(!saysHello);
   };
 
-  console.log("small change!");
+  console.log(process.env.CONFIG_VARIABLE);
 
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
