9bbb1b5e85480e7a825252d5acab2e527dc1b417
samcurteis-sh <146755918+samcurteis-sh@users.noreply.github.com>
2025-08-14 12:23:50 +0100

    ci: test change to only app1

--- apps/app2/app/page.tsx
+++ apps/app2/app/page.tsx
@@ -9,6 +9,7 @@ export default function Home() {
   };
 
   console.log(process.env.CONFIG_VARIABLE);
+  console.log("small change!");
 
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
