56cd486518f7b65e604ad6fdf15c967dffb726fe
samcurteis-sh <146755918+samcurteis-sh@users.noreply.github.com>
2025-08-14 12:58:34 +0100

    ci: change to app2 -> app2 tested, built and deployed

--- apps/app2/app/page.tsx
+++ apps/app2/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   };
 
   console.log(process.env.CONFIG_VARIABLE);
-  console.log("small change again!");
+  console.log("small change once again!");
 
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
