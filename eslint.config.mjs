20ef2ff4f9783676c019281d14d1dcfc93f5a91b
samcurteis-sh <146755918+samcurteis-sh@users.noreply.github.com>
2025-08-14 12:02:43 +0100

    ci: fixed npm run lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,25 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+const config = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: [
+      "**/.next/**", 
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/build/**",
+    ],
+  },
+  {
+    files: ["apps/app1/**/*.{js,jsx,ts,tsx}", "apps/app2/**/*.{js,jsx,ts,tsx}"],
+    languageOptions: {
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ["./apps/app1/tsconfig.json", "./apps/app2/tsconfig.json"],
+      },
+    },
+  },
 ];
 
-export default eslintConfig;
+export default config
